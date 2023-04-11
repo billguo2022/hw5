abba446f240ad385479fc106b80c6ca452b127a0
bill guo <billguo@usc.edu>
2023-04-11 21:47:13 +0000

    complete2

--- schedwork.cpp
+++ schedwork.cpp
@@ -45,12 +45,8 @@ bool schedule(
     dailySched.clear();
 
     // Initialize working days counter
-    std::vector<size_t> workingDays;
-    for (size_t i = 0; i < availability[0].size(); i++){
-
-        workingDays.push_back(0);
-		}
-
+    std::vector<size_t> workingDays(availability[0].size(), 0);
+    
     // Initialize daily schedule with empty worker lists
     for (size_t i = 0; i < availability.size(); i++) {
         std::vector<Worker_T> empty;
@@ -60,6 +56,22 @@ bool schedule(
 
     // Call the helper function to generate the schedule
     return helper(availability, dailyReq, maxShiftsAllowed, dailySched, workingDays, 0);
+}
+
+bool ifworkerassignedtoday(Worker_T workerId,DailySchedule& dailySched,size_t currentDay){
+    bool alreadyAssignedToday = false;
+    if (!dailySched[currentDay].empty()) {
+
+                for (size_t i = 0; i < dailySched[currentDay].size(); i++) {
+
+                    if(dailySched[currentDay][i] == workerId){
+
+                        alreadyAssignedToday = true;
+                        return alreadyAssignedToday;
+                }
+            }
+    }
+    return false;
 }
 
 // Helper function to recursively generate the schedule
@@ -86,19 +98,19 @@ bool helper(
 				
         for (Worker_T workerId = 0; workerId < availability[0].size(); workerId++) {
             // Check if the worker is already assigned for the current day
-            if (!dailySched[currentDay].empty()) {
+            // if (!dailySched[currentDay].empty()) {
 
-                for (size_t i = 0; i < dailySched[currentDay].size(); i++) {
+            //     for (size_t i = 0; i < dailySched[currentDay].size(); i++) {
 
-                    if (dailySched[currentDay][i] == workerId)
+            //         if(dailySched[currentDay][i]= workerId){
 
-                        alreadyAssignedToday = true;
-                }
-            }
+            //             alreadyAssignedToday = true;
+            //     }
+            // }
+            alreadyAssignedToday=ifworkerassignedtoday(workerId,dailySched,currentDay);
             // If the worker is available, hasn't reached max shifts, and is not already assigned, add them to the schedule
             bool x = false;
-            if (availability[currentDay][workerId] == 1 && workingDays[workerId] < maxShiftsAllowed
-                && !alreadyAssignedToday) {
+            if (availability[currentDay][workerId] == 1 && workingDays[workerId] < maxShiftsAllowed && !alreadyAssignedToday) {
 
                 dailySched[currentDay].push_back(workerId);
 
@@ -115,5 +127,6 @@ bool helper(
             }
         }
     }
+    return false;
 }
     // If no valid
