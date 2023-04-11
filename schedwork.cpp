3299ccde7792cd6e7de979b2974d01a9c9c655ee
bill guo <billguo@usc.edu>
2023-04-11 18:41:33 +0000

    comlete

--- schedwork.cpp
+++ schedwork.cpp
@@ -18,27 +18,97 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
+// Helper function for the schedule function
+bool helper(
+    const AvailabilityMatrix& availability,
+    const size_t dailyReq,
+    const size_t maxShiftsAllowed,
+    DailySchedule& dailySched,
+    std::vector<size_t> workingDays,
+    size_t currentDay
+);
 
-// Add prototypes for any helper functions here
-
-
-// Add your implementation of schedule() and other helper functions here
-
+// Main scheduling function
 bool schedule(
-    const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
-    DailySchedule& sched
+    const AvailabilityMatrix& availability,
+    const size_t dailyReq,
+    const size_t maxShiftsAllowed,
+    DailySchedule& dailySched
 )
 {
-    if(avail.size() == 0U){
+    // Return false if there's no availability data
+    if (availability.size() == 0U) {
         return false;
     }
-    sched.clear();
-    // Add your code below
 
+    // Clear the daily schedule
+    dailySched.clear();
 
+    // Initialize working days counter
+    std::vector<size_t> workingDays;
+    for (size_t i = 0; i < availability[0].size(); i++){
 
+        workingDays.push_back(0);
+		}
 
+    // Initialize daily schedule with empty worker lists
+    for (size_t i = 0; i < availability.size(); i++) {
+        std::vector<Worker_T> empty;
+        dailySched.push_back(empty);
+    }
+
+    // Call the helper function to generate the schedule
+    return helper(availability, dailyReq, maxShiftsAllowed, dailySched, workingDays, 0);
 }
 
+// Helper function to recursively generate the schedule
+bool helper(
+    const AvailabilityMatrix& availability,
+    const size_t dailyReq,
+    const size_t maxShiftsAllowed,
+    DailySchedule& dailySched,
+    std::vector<size_t> workingDays,
+    size_t currentDay
+)
+{
+    // Base case: if all days are scheduled, return true
+    if (currentDay >= availability.size()) {
+        return true;
+    }
+    // If the current day's schedule is already filled, move on to the next day
+    else if (dailySched[currentDay].size() >= dailyReq) {
+        return helper(availability, dailyReq, maxShiftsAllowed, dailySched, workingDays, currentDay + 1);
+    }
+    // If the current day's schedule is not yet filled, try to add workers
+    else {
+        bool alreadyAssignedToday = false;
+        for (Worker_T workerId = 0; workerId < availability[0].size(); workerId++) {
+            // Check if the worker is already assigned for the current day
+            if (!dailySched[currentDay].empty()) {
+
+                for (size_t i = 0; i < dailySched[currentDay].size(); i++) {
+
+                    if (dailySched[currentDay][i] == workerId)
+
+                        alreadyAssignedToday = true;
+                }
+            }
+            // If the worker is available, hasn't reached max shifts, and is not already assigned, add them to the schedule
+            bool x = false;
+            if (availability[currentDay][workerId] == 1 && workingDays[workerId] < maxShiftsAllowed
+                && !alreadyAssignedToday) {
+                dailySched[currentDay].push_back(workerId);
+                workingDays[workerId]++;
+                // Recursively call the helper function to fill the schedule
+                x = helper(availability, dailyReq, maxShiftsAllowed, dailySched, workingDays, currentDay);
+                // If the schedule works, return true
+                if (x == true)
+                    return true;
+                // Otherwise, backtrack by removing the worker from the schedule and decrementing their working days
+                dailySched[currentDay].pop_back();
+                workingDays[workerId]--;
+            }
+        }
+    }
+}
+    // If no valid
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -12,17 +13,87 @@
 using namespace std;
 
 
-// Add prototypes of helper functions here
+// Function to generate possible words for a wordle game given an input string,
+// a set of floating characters, and a dictionary.
+void generate_words(std::string& input, std::string& remaining_chars,
+                     size_t index, const std::set<std::string>& dictionary,
+                     size_t num_dashes, std::set<std::string>& results);
 
 
-// Definition of primary wordle function
-std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
+std::set<std::string> wordle(const std::string& input,
+                                           const std::string& floating_chars,
+                                           const std::set<std::string>& dictionary)
 {
-    // Add your code here
+    std::set<std::string> results;
 
+    // Count the number of dashes in the input string
+    int dashn = 0;
+    for (unsigned int i = 0; i < input.length(); i++){
+        if (input[i] == '-') {
+            dashn++;
+        }
+    }
+
+    // Make copies of the input string and floating characters to be able to modify them
+    std::string input_copy = input;
+    std::string floating_copy = floating_chars;
+
+    // Call the recursive helper function to generate all possible words
+    generate_words(input_copy, floating_copy, 0, dictionary, dashn, results);
+
+    return results;
 }
 
-// Define any helper functions here
+// Recursive helper function to generate possible words for a wordle game
+void generate_words(std::string& input, std::string& charRe,
+                     size_t index, const std::set<std::string>& dictionary,
+                     size_t dashn, std::set<std::string>& results)
+{
+    // If the index has reached the end of the input string, check if the current word
+    // is in the dictionary and add it to the set of results if it is.
+    if (index == input.length()) {
+        if (dictionary.find(input) != dictionary.end() && charRe == "") {
+            results.insert(input);
+        }
+        return;
+    }
+
+    // If the number of remaining characters is greater than the number of dashes,
+    // there are not enough dashes to use all the floating characters, so return.
+    if (charRe.length() > dashn) {
+        return;
+    }
+
+    // If the character at the current index is not a dash, move to the next index.
+    if (input[index] != '-') {
+        generate_words(input, charRe, index + 1, dictionary, dashn, results);
+        return;
+    }
+    else {
+        // If the character at the current index is a dash, try each floating character
+        // and recursively call the function to fill the next dash with another floating character.
+        for (char x = 'a'; x <= 'z'; x++) {
+            if (charRe.find(x) != std::string::npos) {
+							
+                // If the floating character is available, use it to fill the current dash
+                input[index] = x;
+
+                charRe.erase(charRe.find(x), 1);
+
+                // Recursively call the function to fill the next dash
+                generate_words(input, charRe, index + 1, dictionary, dashn - 1, results);
+
+                // Undo the changes and restore the floating character
+                input[index] = '-';
+                
+                charRe = charRe + x;
+            }
+            else {
+                // If the floating character is not available, skip to the next one
+                input[index] = x;
+                generate_words(input, charRe, index + 1, dictionary, dashn - 1, results);
+                input[index] = '-';
+            }
+        }
+    }
+}
